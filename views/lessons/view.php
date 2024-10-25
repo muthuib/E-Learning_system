3af2e395f18da795c6681554c87f20ea3f14e420
Benjamin Musungwa Muthui <35784605@AD.UONBI.AC.KE>
2024-10-25 12:09:03 +0300

    implements back buttons

--- views/lessons/view.php
+++ views/lessons/view.php
@@ -14,40 +14,43 @@ $this->title = $model->TITLE;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="lessons-view">
+<div class="text-end mb-3">
+    <!-- Back button -->
+    <?= Html::a('Back to Lessons', ['lessons/index'], ['class' => 'btn btn-primary']) ?>
+</div>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="d-flex justify-content-between mt-3">
-        <?php if (Yii::$app->user->can('admin') || Yii::$app->user->can('instructor')): ?>
-        <a href="<?= Url::to(['view', 'LESSON_ID' => $model->LESSON_ID]) ?>" class="btn btn-info">View</a>
-        <a href="<?= Url::to(['update', 'LESSON_ID' => $model->LESSON_ID]) ?>" class="btn btn-primary">Update</a>
-        <?= Html::a('Delete', ['delete', 'LESSON_ID' => $model->LESSON_ID], [
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => 'Are you sure you want to delete this lesson?',
-                    'method' => 'post',
-                ],
-            ]) ?>
-        <?php elseif (Yii::$app->user->can('student')): ?>
-
-        <?php endif; ?>
-    </div>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'attribute' => 'COURSE_ID',
-                'label' => 'Course Name',
-                'value' => function ($model) {
-                    return $model->cOURSE->COURSE_NAME; // Assuming you have a relation in Enrollments model named 'course'
-                }
+<div class="d-flex justify-content-between mt-3">
+    <?php if (Yii::$app->user->can('admin') || Yii::$app->user->can('instructor')): ?>
+    <a href="<?= Url::to(['view', 'LESSON_ID' => $model->LESSON_ID]) ?>" class="btn btn-info">View</a>
+    <a href="<?= Url::to(['update', 'LESSON_ID' => $model->LESSON_ID]) ?>" class="btn btn-primary">Update</a>
+    <?= Html::a('Delete', ['delete', 'LESSON_ID' => $model->LESSON_ID], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this lesson?',
+                'method' => 'post',
             ],
-            'TITLE',
-            'CONTENT:ntext',
-            'VIDEO_URL:url',
+        ]) ?>
+    <?php elseif (Yii::$app->user->can('student')): ?>
 
+    <?php endif; ?>
+</div>
+
+<?= DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+        [
+            'attribute' => 'COURSE_ID',
+            'label' => 'Course Name',
+            'value' => function ($model) {
+                return $model->cOURSE->COURSE_NAME; // Assuming you have a relation in Enrollments model named 'course'
+            }
         ],
-    ]) ?>
+        'TITLE',
+        'CONTENT:ntext',
+        'VIDEO_URL:url',
+
+    ],
+]) ?>
 
 </div>
